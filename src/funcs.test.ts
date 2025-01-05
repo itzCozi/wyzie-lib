d26b8d88a903deb1229a9fc1609efdc4c193e514
FifthWit <110744759+FifthWit@users.noreply.github.com>
2025-01-05 01:13:13 +0000

    added basic tests with Vitest

---
+++ src/funcs.test.ts
@@ -0,0 +1,28 @@
+import { describe, it, expect } from 'vitest';
+import { parseToVTT, searchSubtitles, SubtitleData } from "./main"
+
+describe('Parse to VTT', () => {
+	it('Should Parse Subtitles to VTT', () => {
+        let subtitle: undefined | SubtitleData;
+        searchSubtitles({ tmdb_id: 2190, season: 1, episode: 1, language: 'en', format: 'srt' }).then((data) => {
+            subtitle = data[0];
+            parseToVTT(subtitle.url).then((vttContent) => {
+                expect(vttContent).toContain('Kick ass.') // Valid line from episode, I also just think its funny to have that as a testcase
+            });
+        });
+	});
+});
+
+describe('Search Subtitles', () => {
+    it('Should Search Subtitles', () => {
+        searchSubtitles({tmdb_id: 2190, season: 1, episode: 1, language: 'en', format: 'srt'}).then((data) => {
+            expect(data).toBeInstanceOf(Array);
+            expect(data.length).toBeGreaterThan(0);
+            data.forEach(item => {
+                expect(item).toHaveProperty('id');
+                expect(item).toHaveProperty('url');
+                expect(item).toHaveProperty('format');
+            });
+        });
+    });
+})
